3cf5cc650781da7e262d2d949fc2e762101ce074
fclopesx <fer_dyp@hotmail.com>
2024-03-27 22:06:28 -0300

    Conteudo aula 27/03

--- NP24_Java2_B/src/br/com/senaisp/bauru/fernanda/licao06/lacofor/TestePoligonos.java
+++ NP24_Java2_B/src/br/com/senaisp/bauru/fernanda/licao06/lacofor/TestePoligonos.java
@@ -3,10 +3,13 @@ package br.com.senaisp.bauru.fernanda.licao06.lacofor;
 public class TestePoligonos {
 
 	public static void main(String[] args) {
-        Poligonos pl = new Poligonos();
-        pl.montarQuadrado();
-        pl.montarTrianguloInferior();
-        pl.montarDiagonalPrincipal();
-    }
+		Poligonos pl = new Poligonos();
+		pl.montarQuadrado();
+		pl.montarTrianguloInferior();
+		pl.montarTrianguloSuperior();
+		pl.montarDiagonalPrincipal();
+		pl.montarDiagonalInvertida();
+		pl.montarX();
+	}
 
 }
---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/fernanda/licao07/exercicio/TesteJogo.java
@@ -0,0 +1,17 @@
+package br.com.senaisp.bauru.fernanda.licao07.exercicio;
+
+public class TesteJogo {
+
+	public static void main(String[] args) {
+		Cartao ct01 = new Cartao();
+		System.out.println(ct01);
+		Cartao ct02 = new Cartao();
+		System.out.println(ct02);
+		//Criando uma maquina
+		MaquinaFliperama
+		
+		
+
+	}
+
+}
